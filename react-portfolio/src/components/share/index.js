4648befb062f2aa25d6ff87873f11fb64e832d00
Nastia Klitsuk <nastiak@codevalue.net>
2017-09-07 00:19:08 +0300

    social links were updated. new protfolio items were added.

--- react-portfolio/src/components/share/index.js
+++ react-portfolio/src/components/share/index.js
@@ -3,10 +3,16 @@ import './Share.css';
 
 const Share = () => {
   return (
-    <div className="shareContainer">
-      <button className="Facebook" />
-      <button className="Twitter" />
-      <button className="LinkedIn" />
+    <div className="ShareContainer">
+      <a href="http://www.facebook.com" target="http://www.facebook.com">
+        <button className="Facebook" />
+      </a>
+      <a href="http://twitter.com/twitter" target="http:/twitter.com/twitter">
+        <button className="Twitter" />
+      </a>
+      <a href="http://www.linkedin.com" target="http://www.linkedin.com">
+        <button className="LinkedIn" />
+      </a>
     </div>
   );
 }
